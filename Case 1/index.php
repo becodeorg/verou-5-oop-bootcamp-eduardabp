6e5b6d5fb5ca7c4c85abe43ad97ae699722aa32f
Eduarda <eduardabpetry@gmail.com>
2024-01-09 16:16:31 +0100

    Create case with classes

--- Case 1/index.php
+++ Case 1/index.php
@@ -1,5 +1,7 @@
 <?php
 
+//without classes
+
 $basket = [
     "bananas" => ["quantity" => 6, "price" => 1, "tax" => 6],
     "apples" => ["quantity" => 3, "price" => 1.5, "tax" => 6],
@@ -23,4 +25,36 @@ function calculateTotalPrice (array $array) {
 calculateTotalPrice($basket);
 
 echo $totalPrice . "<br>";
-echo $totalTax;
+echo $totalTax . "<br>";
+
+// using classes
+
+class Basket
+{
+    public $item;
+    public $quantity;
+    public $price;
+    public $tax;
+
+    public function __construct (int $quantity, float $price, float $tax) {
+        $this->quantity = $quantity;
+        $this->price = $price;
+        $this->tax = $tax;
+    }
+
+    public function getTotal () {
+        return $this->quantity * $this->price;
+    }
+
+    public function getTaxTotal () {
+        return ($this->getTotal() / 100) * $this->tax;
+    }
+}
+
+$bananas = new Basket(6, 1, 6);
+$apples = new Basket(3, 1.5, 6);
+$wine = new Basket(2, 10, 21);
+
+echo $totalPrice = $bananas->getTotal() + $apples->getTotal() + $wine->getTotal() . "<br>";
+
+echo $totalTax = $bananas->getTaxTotal() + $apples->getTaxTotal() + $wine->getTaxTotal() . "<br>";
